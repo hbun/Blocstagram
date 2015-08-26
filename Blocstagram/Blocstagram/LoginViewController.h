4ea03dc9ba1d8459c8d3d981c4689a1351639c31
Husam Al-Ziab <husam.alziab@gmail.com>
2015-08-25 20:28:29 -0700

    Checkpoint 33 complete

---
+++ Blocstagram/Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Husam Al-Ziab on 8/25/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
---
+++ Blocstagram/Blocstagram/LoginViewController.m
@@ -0,0 +1,102 @@
+//
+//  LoginViewController.m
+//  Blocstagram
+//
+//  Created by Husam Al-Ziab on 8/25/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "DataSource.h"
+
+@interface LoginViewController () <UIWebViewDelegate>
+
+@property (nonatomic, weak) UIWebView *webView;
+
+@end
+
+@implementation LoginViewController
+
+NSString *const LoginViewControllerDidGetAccessTokenNotification = @"LoginViewControllerDidGetAccessTokenNotification";
+
+- (NSString *)redirectURI {
+    return @"http://bloc.io";
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    //Create Webview for login:
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.delegate = self;
+    
+    [self.view addSubview:webView];
+    self.webView = webView;
+    
+    self.title = NSLocalizedString(@"Login", @"Login");
+    
+    // Provide Client ID:
+    NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=token", [DataSource instagramClientID], [self redirectURI]];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    if (url) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+    }
+}
+
+// Set the web view's frame:
+- (void) viewWillLayoutSubviews {
+    self.webView.frame = self.view.bounds;
+}
+
+- (void) dealloc {
+    //Removing this line can cause a flickering effect when you relaunch the app after loggin in, as the web view is breifly displayed, automatically authenticates with cookies, returns the access token, and dismisses the login view, sometimes in lesss than a second.
+    [self clearInstagramCookies];
+   
+    self.webView.delegate = nil;
+}
+
+/**
+ Clears Instagram cookies. This prevents caching the credentials in the cookie jar.
+ */
+-(void) clearInstagramCookies {
+    for(NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]) {
+        NSRange domainRange = [cookie.domain rangeOfString:@"instagram.com"];
+        if(domainRange.location != NSNotFound) {
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+        }
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSString *urlString = request.URL.absoluteString;
+    if([urlString hasPrefix:[self redirectURI]]) {
+        //This contains our Auth token
+        NSRange rangeOfAccessTokenParameter = [urlString rangeOfString:@"access_token="];
+        NSUInteger indexOfTokenStarting = rangeOfAccessTokenParameter.location + rangeOfAccessTokenParameter.length;
+        NSString *accessToken = [urlString substringFromIndex:indexOfTokenStarting];
+        [[NSNotificationCenter defaultCenter] postNotificationName:LoginViewControllerDidGetAccessTokenNotification object:accessToken];
+        
+        return NO;
+    }
+    return YES;
+}
+
+@end
