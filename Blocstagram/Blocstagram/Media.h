e14de854e7caa354cf894b164bc80c797ea3f277
Husam Al-Ziab <husam.alziab@gmail.com>
2015-09-02 16:18:01 -0700

    Checkpoint 38 complete

--- Blocstagram/Blocstagram/Media.h
+++ Blocstagram/Blocstagram/Media.h
@@ -6,7 +6,15 @@
 //  Copyright (c) 2015 Bloc. All rights reserved.
 //
 
+//#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, MediaDownloadState) {
+    MediaDownloadStateNeedsImage            = 0,
+    MediaDownloadStateDownloadInProgress    = 1,
+    MediaDownloadStateNonRecoverableError   = 2,
+    MediaDownloadStateHasImage              = 3
+};
 
 @class User;
 
@@ -16,6 +24,8 @@
 @property (nonatomic, strong) User *user;
 @property (nonatomic, strong) NSURL *mediaURL;
 @property (nonatomic, strong) UIImage *image;
+@property (nonatomic, assign) MediaDownloadState downloadState;
+
 @property (nonatomic, strong) NSString *caption;
 @property (nonatomic, strong) NSArray *comments;
 
